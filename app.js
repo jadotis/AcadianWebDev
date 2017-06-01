8dbc7fc825a46cb74a8d4b7c6d0bf37ec320cd6b
James Otis <jadotis@bu.edu>
2017-06-01 16:51:58 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+﻿var express = require('express'); // Express web server framework
+var session = require('express-session');
+var app = express();
+var request = require('request'); // "Request" library
+var handlebars = require('handlebars');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var body_parser = require('body-parser');
+var session = require('express-session');
+var favicon = require('serve-favicon');
+var path = require('path');
+app.use(session({
+    secret: 'ThisKeyCanBeAnything',
+    resave: false,
+    saveUnitialized: true,
+    cookie: {secure: false},
+    access_token: null
+}));
+
+
+app.use(body_parser.urlencoded({extended : false}));
+app.use(body_parser({limit: '50mb'}));
+app.use(body_parser.json());
+app.use(express.static(__dirname + '/public')).use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public' ,'favicon.ico')));
+
+module.exports = app;
+
+console.log('Listening on 8080');
---
+++ build.ps1
@@ -0,0 +1,37 @@
+﻿#This is an install /build script for website/node project:
+
+#Build the dependencies when installed in a new directory:
+
+
+if(!(Test-Path  "." -include "app.js"))
+{
+    throw "Cannot install the dependencies in this directory";
+    exit;
+}
+
+#Check for an installation of node:
+try
+{
+    $version = node -v;
+}
+catch
+{
+   throw "There is no version of Node.js installed"; 
+}
+
+Write-Host "Beginning to install the dependencies for the project";
+#Dependencies
+
+npm install body-parser --save
+npm install cookie-parser --save
+npm install express --save
+npm install express session --save
+npm install handlebars --save
+npm install morgan --save
+npm install pug --save
+npm install request --save
+npm install serve-favicon --save
+
+
+
+
