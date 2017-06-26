0ed6b0914422182f39d1f8e3416207a68524f438
James Otis <jadotis@bu.edu>
2017-06-26 17:21:47 -0400

    Redid entire color scheme per request
    -Added major functionality and added many webkit changes
    -Continual development of tables and scaling issues. 
    -Began Jquery work to append element to JSON and continued to build out the form.
    -Much work ahead

--- public/js/tableController.js
+++ public/js/tableController.js
@@ -10,21 +10,14 @@ app.controller('tableController', function ($scope, $element, $timeout, $http) {
             }
             $scope.serverList = data.servers;
             $scope.array = [];
+            /* Parse the keys for ng-repeat */
             for(var key in $scope.serverList){
                 $scope.array.push($scope.serverList[key]);
             }
             console.log($scope.array);
-
             $scope.sortType = '"name"'; // set the default sort type
             $scope.sortReverse = false;  // set the default sort order
             $scope.searchTerm = '';
-
-
-
-
-
-
-
             //success goes here;
         }).error(function (error) {
         console.log(error);
